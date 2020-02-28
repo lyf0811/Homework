bd7c3c5f50bb0f02dcae3560c5b7461c83f6b05e
lyf0811 <277294976@163.com>
2020-02-28 15:09:26 +0800

    第二次作业

---
+++ No.2-homework1/No.2-homework1/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace No._2_homework1
+{
+   
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = 0;
+            int i = 2;
+            string input;
+            
+            start: try
+            {
+                Console.WriteLine("请输入一个整数数字");
+                input = Console.ReadLine();
+                num = Int32.Parse(input);
+                if (num == 1)
+                {
+                    Console.WriteLine("1没有素数因子");
+                    goto start;
+                }
+                Console.WriteLine("这个数字的所有素数因子为：");
+            }
+            catch
+            {
+                Console.WriteLine("请输入正确的数字");
+                goto start;
+            }
+            while (num>= i)
+            {
+              
+                if (num % i == 0)
+                {
+                    //Console.WriteLine(i);
+                    while (num % i == 0)
+                    {
+                        num /= i;
+                        Console.WriteLine(i);
+                    }
+                }
+                i++;
+            }
+            Console.ReadKey();
+        }
+    }
+    }
+

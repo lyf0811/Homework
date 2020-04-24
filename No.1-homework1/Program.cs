f14a2a7ae112d594498d14558b460030fc9ca482
lyf0811 <277294976@163.com>
2020-04-24 17:20:02 +0800

    第一周作业

---
+++ No.1-homework1/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication16
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                string s = " ";
+                int a = 0, b = 0;
+                try
+                {
+                    Console.Write("请输入第一个数字：");
+                    s = Console.ReadLine();
+                    a = Int32.Parse(s);
+                    Console.Write("请输入第二个数字：");
+                    s = Console.ReadLine();
+                    b = Int32.Parse(s);
+                    int result = 0;
+                    Console.Write("请输入运算符：");
+                    s = Console.ReadLine();
+                }
+                catch (FormatException)
+                {
+                    Console.WriteLine("请输入正确的数字");
+                }
+                catch (OverflowException)
+                {
+                    Console.WriteLine("输入数字溢出");
+                }
+
+                if (s == "+")
+                {
+                    Console.WriteLine($"result:{a + b}");
+                }
+                else if (s == "-")
+                {
+                    Console.WriteLine($"result:{a - b}");
+                }
+                else if (s == "*")
+                {
+                    Console.WriteLine($"result:{a * b}");
+                }
+                else if (s == "/")
+                {
+                    if (b == 0)
+                    {
+                        Console.WriteLine("被除数不能是0");
+                    }
+                    Console.WriteLine($"result:{a / b}");
+                }
+                else if (s != "+" || s != "-" || s != "*" || s != "/")
+                {
+                    Console.WriteLine("输入正确的运算符");
+                }
+                Console.ReadKey();
+            }
+        }
+    }
+}
